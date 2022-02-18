5cb272b04c4e1f41165294a2cbc325ffc72d0b17
BBrandford11 <brandfordbryce@gmail.com>
2022-02-17 19:23:22 -0700

    mocha tests and refactored breed fetcher

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,17 +1,22 @@
 const request = require("request");
-const args = process.argv.slice(2);
-const wantedCat = args[0].toLowerCase().substring(0, 3);
 
-request(
-  `https://api.thecatapi.com/v1/breeds/search?q=${wantedCat}`,
-  (error, response, body) => {
-    // console.log('error:', error); // Print the error if one occurred
-    // console.log('statusCode:', response && response.statusCode); // Print the response status code if a response was received
-    // console.log('body:', body); // Print the HTML for the Google homepage.
-    // console.log(`body is a `)
-    // console.log(typeof body)
-    const data = JSON.parse(body);
-    console.log(data[0]["description"]);
-    //console.log(typeof data);
-  }
-);
+const fetchBreedDescription = function (breed, callback) {
+  const wantedCat = breed.toLowerCase().substring(0, 3);
+
+  const Url = `https://api.thecatapi.com/v1/breeds/search?q=${wantedCat}`;
+  request(Url, (error, response, body) => {
+    if (error) {
+      callback(`Failed to request details. Details: `, null);
+    }
+
+    const data = JSON.parse(body); 
+
+    if (!data[0]) {
+      callback(undefined, undefined);
+    } else {
+      callback(undefined, data[0].description);
+    }
+  });
+};
+
+module.exports = fetchBreedDescription;
